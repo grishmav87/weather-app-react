59e1f3fda41b15b4d56793d3c7dd9f6531158a12
grishmav87 <112604862+grishmav87@users.noreply.github.com>
2023-01-27 21:04:35 +0800

    Added links to display temperature in metric or imperial, moved footer outside App container, and added netlify link

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="weather-app">
-        <WeatherApp defaultCity="Perth" />
-        <footer>
-          <a
-            href="https://github.com/grishmav87/weather-app-react.git"
-            target="blank"
-          >
-            Open-source code
-          </a>{" "}
-          by Grishma V.
-        </footer>
+    <div>
+      {" "}
+      <div className="App">
+        <div className="weather-app">
+          <WeatherApp defaultCity="Perth" />
+        </div>
       </div>
+      <footer>
+        <a
+          href="https://github.com/grishmav87/weather-app-react.git"
+          target="_blank"
+        >
+          Open-source code
+        </a>
+        by Grishma V.{" "}
+        <a href="https://gregarious-pixie-53cac9.netlify.app/" target="_blank">
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CurrentWeather(props) {
-  return (
-    <div className="CurrentWeather">
-      <span className="current-temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">
-        <sup>°C|°F</sup>
-      </span>
-    </div>
-  );
+  const [unit, setUnit] = useState("metric");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("imperial");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("metric");
+  }
+  function imperial() {
+    return props.celsius * 1.8 + 32;
+  }
+
+  if (unit === "metric") {
+    return (
+      <div className="CurrentWeather">
+        <span className="current-temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          <sup>
+            °C|
+            <a href="/" onClick={showFahrenheit}>
+              °F
+            </a>
+          </sup>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="CurrentWeather">
+        <span className="current-temperature">{Math.round(imperial())}</span>
+        <span className="unit">
+          <sup>
+            <a href="/" onClick={showCelsius}>
+              {" "}
+              °C{" "}
+            </a>
+            | °F
+          </sup>
+        </span>
+      </div>
+    );
+  }
 }
